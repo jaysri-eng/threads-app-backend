2bd42b4cf171a23508a0d4052a835e52593b0d5c
jayanthsrinivasan <jayanth.srinivasan@silencespeaks.io>
2025-09-22 20:47:27 +0530

    Schema and postgres db added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@as-integrations/express5';
+import prismaClient from "./lib/db.js";
 
 const app = express();
 const PORT = 3000
@@ -11,14 +12,33 @@ app.get('/',(req,res)=>{
 const server = new ApolloServer({
   typeDefs:`
     type Query {
-        hello: String
+        hello: String,
         say(name:String): String
+    }
+    type Mutation {
+        createUser(email: String!,firstName: String!, lastName: String!,password:String!):Boolean
+
     }
   `, //schema
   resolvers:{
     Query: {
         hello: () => "Hey, hello",
         say:(_,{name}:{name:String})=>`hey ${name}`
+    },
+    Mutation: {
+        createUser: async (_,
+            {firstName, lastName, email,password}:{
+                firstName:string; lastName:string; 
+                email:string;password:string
+            }
+        ) => {
+            await prismaClient.user.create({
+                data: {
+                    email,firstName,lastName,password,salt:"random_salt"
+                }
+            });
+            return true;
+        }
     }
   }, //actions or resolver
 });
